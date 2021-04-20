c9ad8e92e686e6a864c1ba1f9565517e39a6cc97
19020620 <19020620@myrp.edu.sg>
2021-04-20 16:39:02 +0800

    Problem statment of C347 L01

--- app/src/main/java/sg/edu/rp/c346/id19020620/problemstatement/secondActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id19020620/problemstatement/secondActivity.java
@@ -33,12 +33,14 @@ public class secondActivity extends AppCompatActivity {
 
         holiday = new ArrayList<Holiday>();
         if (selectedType.equals("Secular")){
-            holiday.add(new Holiday("Chinese New Year","chinese new year","17/7/2020"));
-            holiday.add(new Holiday("Hari Raya Haji","hari raya haji","17/7/2025"));
-        }
-        else {
             holiday.add(new Holiday("Christmas","christmas","17/7/2018"));
             holiday.add(new Holiday("Labour Day","labour day","17/9/2018"));
+
+
+        }
+        else {
+            holiday.add(new Holiday("Chinese New Year","chinese new year","17/7/2020"));
+            holiday.add(new Holiday("Hari Raya Haji","hari raya haji","17/7/2025"));
         }
 
         aa = new HolidayAdapter(this,R.layout.specificyear,holiday);
